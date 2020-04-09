02c783a4ea2e4993d427da61d1734ececffaf3e5
Ard-Jan van Etten <45192498+arte921@users.noreply.github.com>
2020-04-09 10:05:51 +0200

    polkit-gnome

--- swaysystem.sh
+++ swaysystem.sh
@@ -1,7 +1,7 @@
 #arg 1: pc or laptop, arg 2: username
 
 dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-dnf install -y gstreamer1-plugins-base gstreamer1-plugins-good gstreamer1-plugins-ugly gstreamer1-plugins-bad-free gstreamer1-plugins-bad-free gstreamer1-plugins-bad-freeworld gstreamer1-plugins-bad-free-extras ffmpeg vlc git sway nano firefox gnome-terminal nautilus gedit baobab libreoffice node wine lutris steam playerctl android-tools
+dnf install -y gstreamer1-plugins-base gstreamer1-plugins-good gstreamer1-plugins-ugly gstreamer1-plugins-bad-free gstreamer1-plugins-bad-free gstreamer1-plugins-bad-freeworld gstreamer1-plugins-bad-free-extras ffmpeg vlc git sway nano firefox gnome-terminal nautilus gedit baobab libreoffice node wine lutris steam playerctl android-tools polkit-gnome
 mkdir -p /home/$2/.config/sway
 curl https://raw.githubusercontent.com/arte921/sway-configs/master/$1 > /home/$2/.config/sway/config
 mkdir -p /home/$2/pictures
