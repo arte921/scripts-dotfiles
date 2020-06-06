64392b9c675a83b434e6c06752eb86e70a37c44c
Ard-Jan van Etten <ardjanve@gmail.com>
2020-06-06 23:08:07 +0200

    Update setup-fedora.sh

--- setup-fedora.sh
+++ setup-fedora.sh
@@ -2,7 +2,7 @@ dnf update -y
 dnf install -y git sway nano chromium nodejs playerctl android-tools slurp grim wl-clipboard dkms bc kernel-devel kernel-headers
 curl https://raw.githubusercontent.com/arte921/scripts-dotfiles/master/sway-config > /home/arte/.config/sway/config
 curl https://raw.githubusercontent.com/arte921/scripts-dotfiles/master/.bashrc > /home/arte/.bashrc
-sudo systemctl enable --now sshd.service
+sudo systemctl enable --now sshd
 git clone https://github.com/tomaspinho/rtl8821ce.git
 chown -R arte /home/arte
 echo "reboot, go in rtl8821ce folder, run sudo ./dkms-install.sh, reboot again"
