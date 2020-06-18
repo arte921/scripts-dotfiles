19e065c3fa0276629e8acfb96213455f0e5aafea
Ard-Jan van Etten <ardjanve@gmail.com>
2020-06-18 14:15:36 +0200

    update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,9 +1,7 @@
 mkdir -p ~/.config/sway
-cp sway-config > /home/arte/.config/sway/config
-cp .bashrc > /home/arte/.bashrc
-sudo systemctl enable --now sshd
+cp sway-config /home/arte/.config/sway/config
+cp .bashrc /home/arte/.bashrc
 mkdir -p ~/.keep/sauce/
 cp -r scripts ~/.keep/
 cd ~/.keep/sauce
-git clone https://github.com/tomaspinho/rtl8821ce.git
 git config --global credential.helper store
