03b4d9716bad7d7efb43908e7908b934791107dc
Ard-Jan van Etten <ardjanve@gmail.com>
2020-07-06 14:15:42 +0200

    nix rtl8821ce

--- configuration.nix
+++ configuration.nix
@@ -11,20 +11,7 @@
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  boot.extraModulePackages = with config.boot.kernelPackages;
-    [
-      (rtl8821ce.overrideAttrs (old: {
-        version = "5.5.2_34066.20200325";
-        src = pkgs.fetchFromGitHub {
-          owner = "tomaspinho";
-          repo = "rtl8821ce";
-          rev = "69765eb288a8dfad3b055b906760b53e02ab1dea";
-          sha256 = "17jiw25k74kv5lnvgycvj2g1n06hbrpjz6p4znk4a62g136rhn4s";
-        };
-      }))
-    ];
-
-  boot.kernelModules = [ "rtl8821ce" ];
+  boot.extraModulePackages = with config.boot.kernelPackages; [ rtl8821ce ];
   
   networking.hostName = "redstar";
   networking.networkmanager.enable = true;
@@ -48,7 +35,7 @@
   nixpkgs.config.allowUnfree = true;
 
    environment.systemPackages = with pkgs; [
-    wget vim sway alacritty neofetch git godot bluez-alsa bluez bluez-tools networkmanager grim slurp wl-clipboard mesa radeontop htop libGL ntfs3g j4-dmenu-desktop brightnessctl xwayland brave pavucontrol steam discord lm_sensors micro vscode nano emacs bc chromium playerctl nodejs firefox spidermonkey minetest superTuxKart android-studio lutris imagemagick jetbrains.idea-community xfce.xfce4-whiskermenu-plugin wine winetricks powershell atom sublime3 xfce.xfce4-battery-plugin zerotierone pulseeffects gimp nmap google-chrome arandr kitty unzip zip gnome3.gnome-tweaks blender jdk11 sshfs transmission-gtk vlc
+    wget vim sway alacritty neofetch git godot bluez-alsa bluez bluez-tools networkmanager grim slurp wl-clipboard mesa radeontop htop libGL ntfs3g j4-dmenu-desktop brightnessctl xwayland brave pavucontrol steam discord lm_sensors micro vscode nano emacs bc chromium playerctl nodejs firefox spidermonkey minetest superTuxKart android-studio lutris imagemagick jetbrains.idea-community xfce.xfce4-whiskermenu-plugin wine winetricks powershell atom sublime3 xfce.xfce4-battery-plugin zerotierone pulseeffects gimp nmap google-chrome arandr kitty unzip zip gnome3.gnome-tweaks blender jdk11 sshfs transmission-gtk vlc gjs
   ];
    
    nixpkgs.config.permittedInsecurePackages = [
@@ -81,21 +68,13 @@
   hardware.opengl.driSupport32Bit = true;
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome3.enable = true;  
+  services.xserver.desktopManager.gnome3.enable = true;
+  #services.xserver.desktopManager.xfce.enable = true;
 
   services.xserver.displayManager.gdm.autoLogin.enable = true;
   services.xserver.displayManager.gdm.autoLogin.user = "arte";
   services.xserver.displayManager.defaultSession = "gnome";
-
-  #services.picom.enable = true;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "20.03"; # Did you read the comment?
-
+  
+  system.stateVersion = "20.03"; #no change
 }
 
